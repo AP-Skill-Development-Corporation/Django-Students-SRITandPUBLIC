b1d965e682dc1a82c55553abd87c0a6adf1de511
srikanya659qwe <51402676+srikanya659qwe@users.noreply.github.com>
2021-06-17 23:09:53 -0700

    Day-9
    Day-9

---
+++ Day-9/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def hi(request):
+	return HttpResponse("hai guys welcome to django session")
+
+def hello(request,num):
+	return HttpResponse('my roll number is:{}'.format(num))
+
+def task(request,name,id):
+	return HttpResponse("my name is {} and My roll number is {}".format(name,id))
+
+def basic(request):
+	return render(request,'myApp/basic.html')
+
+def dyn(request,name,id):
+	return render(request,'myApp/dyn.html',{'n':name,'i':id})
+
+def table(request):
+	return render(request,'myApp/table.html')
+
+def inline(request):
+	return render(request,'myApp/inline.html')
+
+def internal(request):
+	return render(request,'myApp/internal.html')
+
+def task2(request,num):
+	data=[]
+	for i in range(1,11):
+		td=str(num)+"*"+str(i)+'='+str(num*i)
+		data.append(td)
+	return render(request,'myApp/task2.html',{'info':data})
+
+def external(request):
+	return render(request,'myApp/external.html')
+
+def boot(request):
+	return render(request,'myApp/boot.html')
+
+def task3(request):
+	return render(request,'myApp/task3.html')
+	
+def offline(request):
+	if request.method=="POST":
+		name=request.POST['uname']
+		email=request.POST['email']
+		mobile=request.POST['mobile']
+		gender=request.POST['optradio']
+		dob=request.POST['dob']
+		lang=request.POST['lan']
+		file=request.POST['file']
+		#print(name,email,mobile,gender,dob,lang,file)
+		return render(request,'myApp/get.html',{'uname':name,'email':email,'mob':mobile,'gender':gender,'db':dob,'lan':lang,'fl':file})
+	return render(request,'myApp/offline.html')
+
+def get(request):
+	return render(request,'myApp/get.html')
